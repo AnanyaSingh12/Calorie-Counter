39163dfb16463906ca7add219bf55765752fbba7
AnanyaSingh12 <68113764+AnanyaSingh12@users.noreply.github.com>
2021-08-18 17:43:33 +0530

    Add files via upload

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -13,19 +13,5 @@ urlpatterns = [
     path('login/',views.loginPage,name='login'),
     path('logout/',views.logoutUser,name='logout'),
     path('addFooditem/',views.addFooditem,name='addFooditem'),
-     path('reset_password/',
-         auth_views.PasswordResetView.as_view(),
-         name="reset_password"),
 
-    path('reset_password_sent/',
-         auth_views.PasswordResetDoneView.as_view(),
-         name="password_reset_done"),
-
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name="password_reset_confirm"),
-
-    path('reset_password_complete/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name="password_reset_complete"),
 ]
